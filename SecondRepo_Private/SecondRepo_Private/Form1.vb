f7d2af6173e6f4dcfe2c4aa63a841eb7be719d20
hamid <hamidagm7@gmail.com>
2021-08-29 20:12:07 +0100

    Creating a private repo

---
+++ SecondRepo_Private/SecondRepo_Private/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Private for Github", MsgBoxStyle.Information, "Info")
+    End Sub
+End Class
