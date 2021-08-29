906bac1fbef0dbd5128720a7b2958389e5f122e7
hamid <hamidagm7@gmail.com>
2021-08-29 20:04:12 +0100

    Adding a simple messagebox !

--- FirstRepo00/FirstRepo00/Form1.vb
+++ FirstRepo00/FirstRepo00/Form1.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Form1
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Just for Github", MsgBoxStyle.Information, "Info")
+    End Sub
 End Class
